5cea53910a819e68ffe4f95c202f9ceef9e3ccf9
Pedro Marcos Ramos <53919658+P3d50@users.noreply.github.com>
2020-07-04 19:25:42 -0300

    adiconando desafio

---
+++ JAVALOGICA/src/introducao/Desafio.java
@@ -0,0 +1,16 @@
+package introducao;
+
+class Desafio {
+	
+	public static void main(String args[]){
+		
+		System.out.println(" ________     ________    ________      _________     __________ " );
+		System.out.println("|   __  \\   |  ______|  |  ____  \\   |  ______\\   /  _______ \\");
+		System.out.println("|  |  |   |  |  |        |  |   \\  |  |  |    \\ |  |  |      | |");
+		System.out.println("|   --   /   |  ------   |  |     | |  |  |_____/ /  |  |      | |");
+		System.out.println("|  -----     |  ______|  |  |     | |  |      ___/   |  |      | |");
+		System.out.println("|  |         |  |        |  |    // |  |     \\      |  |______| |");
+		System.out.println("|  |         |  ------|  |  |__ // /   |   _  \\     |           |");
+		System.out.println("|__|         |________|  |________/    |__| |__\\    \\_________/" );
+	}
+}

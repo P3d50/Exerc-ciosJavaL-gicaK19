f64582428c142fb7e9149ff86933a69d29abb009
noname <rpedromarcos@gmail.com>
2020-07-04 19:51:17 -0300

    comentário explicatic método imprime nome

--- JAVALOGICA/src/introducao/Desafio.java
+++ JAVALOGICA/src/introducao/Desafio.java
@@ -7,14 +7,16 @@ class Desafio {
 		imprimeNome();
 	}
 	
+	//desta forma esta imprimindo de uma forma que as letras ficam certas, tem que ficar
+	//desse jeito no código por causa do escape que esta sendo usado para desenhar as letras
 	public static void imprimeNome() {
 		System.out.println(" ________     ________    ________      _________     __________ " );
-		System.out.println("|   __  \\   |  ______|  |  ____  \\   |  ______\\   /  _______ \\");
-		System.out.println("|  |  |   |  |  |        |  |   \\  |  |  |    \\ |  |  |      | |");
-		System.out.println("|   --   /   |  ------   |  |     | |  |  |_____/ /  |  |      | |");
-		System.out.println("|  -----     |  ______|  |  |     | |  |      ___/   |  |      | |");
-		System.out.println("|  |         |  |        |  |    // |  |     \\      |  |______| |");
-		System.out.println("|  |         |  ------|  |  |__ // /   |   _  \\     |           |");
-		System.out.println("|__|         |________|  |________/    |__| |__\\    \\_________/" );
+		System.out.println("|   __  \\    |  ______|  |  ____  \\    |  ______ \\    /  _______ \\");
+		System.out.println("|  |  |  |   |  |        |  |   \\  |   |  |     \\ |  |  |      | |");
+		System.out.println("|   --   /   |  ------   |  |    | |   |  |_____/ /  |  |      | |");
+		System.out.println("|  -----     |  ______|  |  |    | |   |      ___/   |  |      | |");
+		System.out.println("|  |         |  |        |  |    / |   |     \\       |  |______| |");
+		System.out.println("|  |         |  ------|  |  |__ /  /   |   _  \\      |           |");
+		System.out.println("|__|         |________|  |________/    |__| |__\\      \\_________/" );
 	}
 }

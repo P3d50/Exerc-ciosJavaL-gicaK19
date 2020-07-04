87a79b44c2db0e2d0317f124767ac40b302d9a66
noname <rpedromarcos@gmail.com>
2020-07-04 20:41:04 -0300

    finalizando desafio 1 e 2 da introdução

--- JAVALOGICA/src/introducao/Desafio.java
+++ JAVALOGICA/src/introducao/Desafio.java
@@ -5,8 +5,9 @@ class Desafio {
 	public static void main(String args[]){
 		
 		imprimeNome();
+		imprimeASC2Char(1200);
 	}
-	
+	//desafio1
 	//desta forma esta imprimindo de uma forma que as letras ficam certas na execução do método, tem que ficar
 	//desse jeito no código por causa do escape que esta sendo usado para desenhar as letras
 	public static void imprimeNome() {
@@ -18,5 +19,18 @@ class Desafio {
 		System.out.println("|  |         |  |        |  |    / |   |     \\       |  |______| |");
 		System.out.println("|  |         |  ------|  |  |__ /  /   |   _  \\      |           |");
 		System.out.println("|__|         |________|  |________/    |__| |__\\      \\_________/" );
+	}
+	//desafio2
+	//adicionando asc2
+	public static void imprimeASC2Char(int end) {
+		int coluna = 0;
+		for(int i = 0; i<= end;i++) {
+			coluna++;
+			System.out.print((char)i+",");
+			if(coluna == 50) {
+				coluna = 0;
+				System.out.println("");
+			}
+		}
 	}
 }	
